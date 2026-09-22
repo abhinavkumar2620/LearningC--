bc847b4379c01a453096e49900697c9d8539c3d4
abhinavkumar4535-prog <abhinavkumar4535@gmail.com>
2026-09-22 18:21:54 +0530

    basic of the coding

---
+++ SquireOFNumber.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a; 
+cout<<"enter a number for squaring"<<endl;
+cin>>a;
+int b = a * a;
+cout<<b<<endl;
+
+
+}
---
+++ productOFThreeNumber.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int a, b, c, d;
+  cout << "enter the first number ";
+  cin >> a;
+  cout << "Enter the second number ";
+  cin >> b;
+  cout << "Enter The Third number ";
+  cin >> c;
+  cout << "The Product Of Three Number" << a * b * c << endl;
+}
