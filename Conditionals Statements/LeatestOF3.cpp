585df4b34f8eb3e2824e8216e48783e5c8b441cc
abhinavkumar4535-prog <abhinavkumar4535@gmail.com>
2026-09-22 20:16:04 +0530

    least of three number

---
+++ Conditionals Statements/LeatestOF3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int a, b, c;
+  cout << "entre the first   = ";
+  cin >> a;
+  cout << "enter the second   = ";
+  cin >> b;
+  cout << "enter the third   = ";
+  cin >> c;
+  if ((a < b) and (a < c))
+  {
+    cout << "then  a is the least" << endl;
+  }
+  else if (b < c and b < a)
+  {
+    cout << "then  a is the least" << endl;
+  }
+  else
+  {
+    cout << "then c is the least" << endl;
+  }
+}
